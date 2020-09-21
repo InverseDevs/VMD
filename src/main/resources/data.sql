edb8166541eea31b5f95746d9bedfa899d5e1198
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-09-21 23:14:38 +0300

    Basic backend authorization added

--- src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-insert into users (id, name) values ('1', 'Alex');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,15 @@
-create table if NOT exists users (
-id varchar(32),
-name varchar(32)
+create table if not exists t_user(
+  id       int,
+  username varchar(256),
+  password varchar(256)
+);
+
+create table if not exists t_role(
+  id       int,
+  name     varchar(256)
+);
+
+create table if not exists t_user_roles(
+  user_id int,
+  role_id int
 );
