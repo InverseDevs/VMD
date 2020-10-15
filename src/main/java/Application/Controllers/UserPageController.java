5753446aa445c6ca33613ca479bfd3615076332b
Alex Egorowski <ad3fful@yandex.ru>
2020-10-15 16:53:27 +0300

    Updated UserPage

--- src/main/java/Application/Controllers/UserPageController.java
+++ src/main/java/Application/Controllers/UserPageController.java
@@ -1,8 +1,11 @@
 package Application.Controllers;
 
 import Application.Content.UserInfo;
+import Application.Content.WallPost;
+import Application.Entities.User;
 import Application.Services.UserInfoService;
 import Application.Services.UserService;
+import Application.Services.WallPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,19 +15,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class UserPageController {
     private UserInfoService infoService;
+    private WallPostService postService;
+    private UserService userService;
 
     @Autowired
-    public UserPageController(UserInfoService infoService) {
+    public UserPageController(UserInfoService infoService, WallPostService postService, UserService userService) {
         this.infoService = infoService;
+        this.postService = postService;
+        this.userService = userService;
     }
 
     @GetMapping("/user/{token}")
     public String userPage(@PathVariable("token") String token, Model model) {
         UserInfo info;
-        if(token.startsWith("id")) info = infoService.findUserInfo(Long.parseLong(token.substring(2)));
-        else info = infoService.findUserInfo(token);
+        User user;
+
+        if(token.startsWith("id")) user = userService.findUserById(Long.parseLong(token.substring(2)));
+        else user = (User) userService.loadUserByUsername(token);
+
+        info = infoService.findUserInfo(user.getId());
+        Iterable<WallPost> posts = postService.allUserpagePosts(user.getId());
 
         model.addAttribute("curr_info", info);
+        model.addAttribute("posts", posts);
         return "userPage";
     }
 }
