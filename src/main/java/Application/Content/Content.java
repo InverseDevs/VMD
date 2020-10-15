a662b1a924a2aaf3dac0a25474309119a6594191
Alex Egorowski <ad3fful@yandex.ru>
2020-10-15 16:52:07 +0300

    Update Content.java

--- src/main/java/Application/Content/Content.java
+++ src/main/java/Application/Content/Content.java
@@ -3,10 +3,13 @@ package Application.Content;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class Content {
     private Long id;
     private String sender;
     private String content;
+    private Date sentTime;
 }
