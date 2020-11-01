0dc5008f4d0178b165053833150d465083f90213
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2020-11-01 15:09:25 +0300

    Add files via upload

---
+++ react/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ChatApp from './chat/components/ChatApp/ChatApp';
+import ProfileApp from './profile/components/ProfileApp/ProfileApp';
+import {BrowserRouter, Route} from 'react-router-dom';
+class App extends React.Component{
+    render(){
+        return (
+            <BrowserRouter>
+                <Route exact path="/chat" component={ChatApp}/>
+                <Route exact path="/" component={ProfileApp}/>
+            </BrowserRouter>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
