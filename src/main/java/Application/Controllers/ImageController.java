4a15e29a0ca19c96c866c615bec6cca85ca9facb
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-05 22:49:11 +0300

    Image controller added

--- src/main/java/Application/Controllers/ImageController.java
+++ src/main/java/Application/Controllers/ImageController.java
@@ -1,20 +1,24 @@
 package Application.Controllers;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @Controller
 public class ImageController {
 
     @RequestMapping(method = RequestMethod.POST)
-    public String importImage(@RequestParam("image") MultipartFile multipartFile) {
-        System.out.println(multipartFile.getName());
-        System.out.println(multipartFile.getOriginalFilename());
-        System.out.println(multipartFile.getContentType());
+    public String importImage(@RequestParam("image") MultipartFile multipartFile, Model model) throws IOException {
+        byte[] bytes = Base64.encodeBase64(multipartFile.getBytes());
 
-        return "home";
+        model.addAttribute("bytes", new String(bytes));
+
+        return "image";
     }
 }
