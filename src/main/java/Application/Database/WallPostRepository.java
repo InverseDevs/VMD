d944cdcf311325bc5e16f76ff56d5b86b7aee055
Alex Egorowski <ad3fful@yandex.ru>
2020-11-05 19:13:35 +0300

    Made WallPostRepository a JPA repository

--- src/main/java/Application/Database/WallPostRepository.java
+++ src/main/java/Application/Database/WallPostRepository.java
@@ -1,11 +1,11 @@
 package Application.Database;
 
 import Application.Entities.Content.WallPost;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface WallPostRepository extends CrudRepository<WallPost, Long> {
+public interface WallPostRepository extends JpaRepository<WallPost, Long> {
 
     @Query("SELECT p from WallPost p where p.pageType = :pageType and p.pageId = :pageId")
     Iterable<WallPost> findByPage(@Param("pageId") Long pageId, @Param("pageType") WallPost.PageType pageType);
