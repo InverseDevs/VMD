4367702eb8202d0afa627618736ebd78c6015ad7
Alex Egorowski <ad3fful@yandex.ru>
2020-11-05 16:43:36 +0300

    Made UserInfoRepository a JPARepository

--- src/main/java/Application/Database/UserInfoRepository.java
+++ src/main/java/Application/Database/UserInfoRepository.java
@@ -1,9 +1,10 @@
 package Application.Database;
 
 import Application.Entities.User.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserInfoRepository extends CrudRepository<UserInfo, Long> {
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
     UserInfo findByUsername(String username);
     UserInfo deleteByUsername(String username);
 }
