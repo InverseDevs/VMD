4ff7462e8739f9f04fdd1f7c01edbf32dfc276d4
Alex Egorowski <ad3fful@yandex.ru>
2020-11-09 17:08:12 +0300

    Added sample chats and messages

--- src/main/java/Application/Starter.java
+++ src/main/java/Application/Starter.java
@@ -1,10 +1,16 @@
 package Application;
 import Application.Database.WallPostRepository;
+import Application.Entities.Chat;
+import Application.Entities.Content.ChatMessage;
 import Application.Entities.Content.WallPost;
 import Application.Database.RoleRepository;
 import Application.Database.User.UserRepository;
 import Application.Entities.Role;
 import Application.Entities.User;
+import Application.Services.ChatService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,8 +19,10 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 
 @SpringBootApplication
+@Slf4j
 public class Starter {
     public static final String homeLink = "http://inversedevs.herokuapp.com";
     //public static final String homeLink = "http://localhost:8080";
@@ -25,6 +33,10 @@ public class Starter {
     RoleRepository roleRepo;
     @Autowired
     WallPostRepository postRepo;
+    @Autowired
+    ChatService chatService;
+
+    Logger logger = LoggerFactory.getLogger(Starter.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Starter.class, args);
@@ -62,6 +74,15 @@ public class Starter {
             posts.add(new WallPost(users.get(2), "hey twin!", new Date(), 2L, type));
             posts.add(new WallPost(users.get(1), "hi there!", new Date(), 3L, type));
             posts.forEach(postRepo::save);
+
+            Chat p2pChat = chatService.getChat(admin, users.get(2));
+            chatService.saveMessage(new ChatMessage("Hello admin", new Date(), users.get(2), p2pChat));
+            chatService.saveMessage(new ChatMessage("Hello skelantros!", new Date(), admin, p2pChat));
+
+            Chat multiChat = chatService.getChat(new HashSet<>(users));
+            chatService.saveMessage(new ChatMessage("hey y'all!", new Date(), users.get(2), multiChat));
+            chatService.saveMessage(new ChatMessage("hey dude!", new Date(), users.get(0), multiChat));
+            chatService.saveMessage(new ChatMessage("it's a multi chat test", new Date(), users.get(1), multiChat));
         };
     }
 }
