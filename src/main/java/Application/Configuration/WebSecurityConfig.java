a84367b106687c116ad801b1742ebbba07b86e11
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-13 15:26:24 +0300

    fix

--- src/main/java/Application/Configuration/WebSecurityConfig.java
+++ src/main/java/Application/Configuration/WebSecurityConfig.java
@@ -19,6 +19,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity
                 .csrf()
                 .disable()
+                .cors()
+                .disable()
                 .authorizeRequests()
                 .antMatchers("/registration").not().fullyAuthenticated()
                 .antMatchers("/admin/**").hasRole("ADMIN")
