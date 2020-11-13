42ab32d16bec0e34c2d6c280cd4e6e7f640b9bb5
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-13 16:00:54 +0300

    fix

--- src/main/java/Application/Configuration/MvcConfig.java
+++ src/main/java/Application/Configuration/MvcConfig.java
@@ -14,9 +14,4 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         //registry.addViewController("/login").setViewName("login");
     }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
 }
