4adb38c4958da3bf0f77202db3a763b152a0409b
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-13 15:33:15 +0300

    fix

--- src/main/java/Application/Configuration/MvcConfig.java
+++ src/main/java/Application/Configuration/MvcConfig.java
@@ -1,14 +1,22 @@
 package Application.Configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         //registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
