db18e9ed67123e642e94104f66c16ec5c9c077c0
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-20 23:23:38 +0300

    friends declining added

--- src/main/java/Application/Controllers/FriendsController.java
+++ src/main/java/Application/Controllers/FriendsController.java
@@ -179,6 +179,59 @@ public class FriendsController {
         return responseJson.toString();
     }
 
+    @RequestMapping(value = "/friends/decline/{userToDeclineId}", method = RequestMethod.POST)
+    @ResponseBody
+    public String declineFriend(@PathVariable("userToDeclineId") Long userToDeclineId, HttpServletRequest request) {
+        JSONObject responseJson = new JSONObject();
+        try {
+            String header = request.getHeader("Authorization");
+            if (header == null) {
+                throw new MissingRequestHeaderException("Authorization", null);
+            }
+            String jwt = header.substring(7);
+
+            if (JwtProvider.validateToken(jwt)) {
+                StringBuilder data = new StringBuilder();
+                String line;
+                while ((line = request.getReader().readLine()) != null) {
+                    data.append(line);
+                }
+
+                JSONObject jsonObject = new JSONObject(data.toString());
+                long currentUserId = jsonObject.getLong("id");
+                User user = userService.findUserById(currentUserId);
+                User friend = userService.findUserById(userToDeclineId);
+
+                if (!user.getId().equals(friend.getId()) && !userService.friendExists(user, friend)) {
+
+                    userService.deleteFriendRequest(user, friend);
+
+                    responseJson.put("status", "success");
+                } else {
+                    log.info("cannot create friend");
+                    responseJson.put("status", "cannot create friend");
+                }
+            } else {
+                log.info("user not authorized");
+                responseJson.put("status", "user not authorized");
+            }
+        } catch (MissingRequestHeaderException e) {
+            log.error("incorrect request headers: " + e.getMessage());
+            responseJson.put("status", "incorrect request headers");
+        } catch (JSONException | IOException e) {
+            log.error("incorrect request body: " + e.getMessage());
+            responseJson.put("status", "incorrect request body");
+        } catch (UsernameNotFoundException e) {
+            log.error("user not found: " + e.getMessage());
+            responseJson.put("status", "user not found");
+        } catch (Exception e) {
+            log.error("unknown error: " + e.getMessage());
+            responseJson.put("status", "unknown error");
+        }
+
+        return responseJson.toString();
+    }
+
     @RequestMapping(value = "/friends/remove/{id}", method = RequestMethod.POST)
     @ResponseBody
     public String removeFriend(@PathVariable("id") Long id, HttpServletRequest request) {
