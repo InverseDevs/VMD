dff71c4b1aa1f287dd09f7b4b54457bdd3fbdced
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-22 13:09:10 +0300

    chat test

--- src/main/java/Application/Controllers/SocketController.java
+++ src/main/java/Application/Controllers/SocketController.java
@@ -5,10 +5,8 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
-@CrossOrigin(origins = "*", allowedHeaders = "*", exposedHeaders = "Authorization")
 public class SocketController {
 
     @MessageMapping("/user-all")
