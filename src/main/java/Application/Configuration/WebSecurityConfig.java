086378460a8fa8bec01c25f53938bf90ed39d6a9
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-24 08:15:17 +0300

    chat test

--- src/main/java/Application/Configuration/WebSecurityConfig.java
+++ src/main/java/Application/Configuration/WebSecurityConfig.java
@@ -3,12 +3,10 @@ package Application.Configuration;
 import Application.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -60,11 +58,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/");
 
         httpSecurity.headers().frameOptions().disable();
-
-        httpSecurity
-                .addFilterBefore(new SimpleCORSFilter(), LogoutFilter.class)
-                .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS, "/*").permitAll();
     }
 
     @Autowired
