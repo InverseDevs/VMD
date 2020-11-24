155bc2645510b4e7a88cbe8fbee4a16a82b93509
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-24 21:57:01 +0300

    chat test

--- src/main/java/Application/Configuration/SimpleCORSFilter.java
+++ src/main/java/Application/Configuration/SimpleCORSFilter.java
@@ -26,6 +26,7 @@ public class SimpleCORSFilter implements Filter {
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
         ((HttpServletResponse) servletResponse).addHeader("Authorization","*");
+        ((HttpServletResponse) servletResponse).addHeader("authorization","*");
 
         HttpServletResponse resp = (HttpServletResponse) servletResponse;
 
