e3281bf28fe5362c32483b5174db17b09b166446
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-25 12:43:28 +0300

    clean up

--- src/main/java/Application/Configuration/SimpleCORSFilter.java
+++ src/main/java/Application/Configuration/SimpleCORSFilter.java
@@ -7,39 +7,18 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
 
 @Component
 @Slf4j
 public class SimpleCORSFilter implements Filter {
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        log.info("Initilisation du Middleware");
-    }
+    public void init(FilterConfig filterConfig) throws ServletException {}
 
     @Override
     public void doFilter(ServletRequest servletRequest,
                          ServletResponse servletResponse,
                          FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-
-        log.info("start");
-
-        Enumeration<String> requestHeaderNames = request.getHeaderNames();
-        while (requestHeaderNames.hasMoreElements()) {
-            String header = requestHeaderNames.nextElement();
-            log.info("request header name" + header + " request header " + request.getHeader(header));
-        }
-
-        List<String> responseHeaderNames = new ArrayList<>(((HttpServletResponse) servletResponse).getHeaderNames());
-        for (String responseHeaderName : responseHeaderNames) {
-            log.info("response header name " + responseHeaderName +
-                    " response header " + ((HttpServletResponse) servletResponse).getHeader(responseHeaderName));
-        }
-
-        log.info("stop");
 
         ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Origin", "https://verymagicduck.netlify.app");
         ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Credentials", "true");
@@ -50,7 +29,5 @@ public class SimpleCORSFilter implements Filter {
     }
 
     @Override
-    public void destroy() {
-
-    }
+    public void destroy() {}
 }
