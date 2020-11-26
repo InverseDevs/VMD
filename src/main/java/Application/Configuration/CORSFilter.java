1f574d8704a1351ac254a49506d851ad47d64b4d
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-11-26 18:04:47 +0300

    test

--- src/main/java/Application/Configuration/CORSFilter.java
+++ src/main/java/Application/Configuration/CORSFilter.java
@@ -7,6 +7,9 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 @Component
 @Slf4j
@@ -20,10 +23,28 @@ public class CORSFilter implements Filter {
                          FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
 
+        log.info("start");
+
+        Enumeration<String> requestHeaderNames = request.getHeaderNames();
+        while (requestHeaderNames.hasMoreElements()) {
+            String header = requestHeaderNames.nextElement();
+            log.info("request header name" + header + " request header " + request.getHeader(header));
+        }
+
+        List<String> responseHeaderNames = new ArrayList<>(((HttpServletResponse) servletResponse).getHeaderNames());
+        for (String responseHeaderName : responseHeaderNames) {
+            log.info("response header name " + responseHeaderName +
+                    " response header " + ((HttpServletResponse) servletResponse).getHeader(responseHeaderName));
+        }
+
+        log.info("stop");
+
         ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Origin", "https://verymagicduck.netlify.app");
         ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Credentials", "true");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers", "Authorization");
+
+
 
         filterChain.doFilter(request, servletResponse);
     }
