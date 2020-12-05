5d5f7afcb8ced756489c912916661bf5fe73362f
Alex Egorowski <ad3fful@yandex.ru>
2020-12-05 13:21:54 +0300

    Update CustomGroupImpl.java
    Fixed some logic problems.

--- src/main/java/Application/Database/Group/CustomGroupImpl.java
+++ src/main/java/Application/Database/Group/CustomGroupImpl.java
@@ -19,42 +19,42 @@ public class CustomGroupImpl implements CustomGroup {
     @Override
     public Group addMembers(Group group, Set<User> members) {
         Group groupDb = groupInDb.apply(group);
-        groupDb.getMembers().addAll(usersFromDb.apply(members));
+        usersFromDb.apply(members).forEach(groupDb::addMember);
         return groupDb;
     }
 
     @Override
     public Group removeMembers(Group group, Set<User> members) {
         Group groupDb = groupInDb.apply(group);
-        groupDb.getMembers().removeAll(usersFromDb.apply(members));
+        usersFromDb.apply(members).forEach(groupDb::removeMember);
         return groupDb;
     }
 
     @Override
     public Group addAdministrators(Group group, Set<User> administrators) {
         Group groupDb = groupInDb.apply(group);
-        groupDb.getAdministrators().addAll(usersFromDb.apply(administrators));
+        usersFromDb.apply(administrators).forEach(groupDb::addAdministrator);
         return groupDb;
     }
 
     @Override
     public Group removeAdministrators(Group group, Set<User> administrators) {
         Group groupDb = groupInDb.apply(group);
-        groupDb.getAdministrators().removeAll(usersFromDb.apply(administrators));
+        usersFromDb.apply(administrators).forEach(groupDb::removeAdministrator);
         return groupDb;
     }
 
     @Override
     public Group banUsers(Group group, Set<User> banned) {
         Group groupDb = groupInDb.apply(group);
-        groupDb.getBannedUsers().addAll(usersFromDb.apply(banned));
+        usersFromDb.apply(banned).forEach(groupDb::banUser);
         return groupDb;
     }
 
     @Override
     public Group unbanUsers(Group group, Set<User> banned) {
         Group groupDb = groupInDb.apply(group);
-        groupDb.getBannedUsers().removeAll(usersFromDb.apply(banned));
+        usersFromDb.apply(banned).forEach(groupDb::unbanUser);
         return groupDb;
     }
 }
