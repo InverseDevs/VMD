2275f2f9e460019cf783c5c8f8b3013c0b7ad320
Alex Egorowski <ad3fful@yandex.ru>
2020-12-09 12:24:54 +0300

    Update Role.java
    Added equals() and hashCode() methods.

--- src/main/java/Application/Entities/Role.java
+++ src/main/java/Application/Entities/Role.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 @Getter
@@ -42,5 +43,15 @@ public class Role implements GrantedAuthority {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Role && ((Role) o).id.equals(this.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
