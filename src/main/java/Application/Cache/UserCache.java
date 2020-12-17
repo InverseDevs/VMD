ac1e2603952b54559b492a59a6d4c296fbd5a6ff
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-12-17 23:36:40 +0300

    user cache fix

--- src/main/java/Application/Cache/UserCache.java
+++ src/main/java/Application/Cache/UserCache.java
@@ -4,8 +4,10 @@ import Application.Entities.User;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 
+import java.util.concurrent.TimeUnit;
+
 public class UserCache {
-    private static final Cache<Long, User> userCache = CacheBuilder.newBuilder().build();
+    private static final Cache<Long, User> userCache = CacheBuilder.newBuilder().expireAfterAccess(5, TimeUnit.MINUTES).build();
 
     public static User getUser(Long userId) {
         return userCache.getIfPresent(userId);
