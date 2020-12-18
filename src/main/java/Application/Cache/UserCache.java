dac0080be65fdf6d1e7291845712c52cf85d5ead
Alexander Kolomiets <kaa5843771@yandex.ru>
2020-12-18 06:54:13 +0300

    user cache fix

--- src/main/java/Application/Cache/UserCache.java
+++ src/main/java/Application/Cache/UserCache.java
@@ -1,24 +1,34 @@
 package Application.Cache;
 
+import Application.Entities.Role;
 import Application.Entities.User;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class UserCache {
-    private static final Cache<Long, User> userCache = CacheBuilder.newBuilder().expireAfterAccess(5, TimeUnit.MINUTES).build();
+    private static final Cache<Long, UserAdapter> userCache = CacheBuilder.newBuilder().expireAfterAccess(5, TimeUnit.MINUTES).build();
 
     public static User getUser(Long userId) {
-        return userCache.getIfPresent(userId);
+        UserAdapter u = userCache.getIfPresent(userId);
+        User user = new User();
+        user.setRoles(u.roles);
+
+        return user;
     }
 
     public static void cacheUser(User user) {
-        User u = new User();
-        u.setRoles(user.getRoles());
+        UserAdapter u = new UserAdapter();
+        u.roles = user.getRoles();
 
         userCache.put(user.getId(), u);
+    }
+
+    static class UserAdapter {
+        private Set<Role> roles;
     }
 }
